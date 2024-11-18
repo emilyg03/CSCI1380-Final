e38f736cdd6d291497cd06734800b271c208d4d6
emilyg03 <130710648+emilyg03@users.noreply.github.com>
2024-11-18 16:44:53 +0000

    setup

--- snake_game.py
+++ snake_game.py
@@ -1,4 +1,46 @@
-# test pygame installation
+# # test pygame installation
+# import pygame
+# pygame.init()
+# print(pygame.ver)
+
+# game enviorment
 import pygame
 pygame.init()
-print(pygame.ver)
+
+# game window
+
+
+# settings (colors)
+
+
+# gameloop
+
+
+# fps control
+
+
+# snake object
+
+
+# apple object
+
+
+# drawing snake and apple
+
+
+# controlling snake 
+
+
+# position updates
+
+
+# collision checks
+
+
+# what hapens snake eats apple
+
+
+# score
+
+
+# game end / over
